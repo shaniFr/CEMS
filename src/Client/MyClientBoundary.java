70f55afe2baa9b1a2c5d70fe9713240a57558985
ayalachn <81107065+ayalachn@users.noreply.github.com>
2021-04-26 21:18:45 +0300

    proj 3

---
+++ src/Client/MyClientBoundary.java
@@ -0,0 +1,12 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MyClientBoundary {
+
+    @FXML
+    private Button btnInsert;
+
+    @FXML
+    private Button btnUpdate;
+
+}
