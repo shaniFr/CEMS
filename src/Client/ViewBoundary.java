5f38e65310ec3dd349e7b89881c0a99ba5b17d97
yarden adika <yadika14@gmail.com>
2021-04-28 18:01:15 +0300

    test

--- src/Client/ViewBoundary.java
+++ src/Client/ViewBoundary.java
@@ -4,22 +4,47 @@ import java.util.ArrayList;
 
 import Data.Exam;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 public class ViewBoundary {
 
-    @FXML
-    private Button btnBack;
+	@FXML
+	private Button btnBack;
 
-    @FXML
-    private TableView<?> tblView;
-    
-    /* shows message received by the server */
-    public static void showMessage(Object msg) {
-    	ArrayList<Exam> examTable = (ArrayList<Exam>)msg;
-    	
-    	/* todo: gui should show this table .. */
-    	
-    }
+	@FXML
+	private TableView<?> tblView;
+
+	@FXML
+	private TableColumn<?, ?> ExamID;
+
+	@FXML
+	private TableColumn<?, ?> Subject;
+
+	@FXML
+	private TableColumn<?, ?> Course;
+
+	@FXML
+	private TableColumn<?, ?> Duration;
+
+	@FXML
+	private TableColumn<?, ?> QScore;
+
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/gui/ViewDataBase.fxml"));
+
+		Scene scene = new Scene(root);
+		// scene.getStylesheets().add(getClass().getResource("/gui/AcademicFrame.css").toExternalForm());
+		ExamID.setCellValueFactory(ArrayList<Exam>("ExamID"));
+
+		primaryStage.setTitle("View DataBase");
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
+	}
 }
