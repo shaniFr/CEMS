cad6948335e49325a6a1e1d43d8de3a6efbf54a1
ayalachn <81107065+ayalachn@users.noreply.github.com>
2021-04-30 17:58:40 +0300

    gui

--- src/Server/MyServerBoundary.java
+++ src/Server/MyServerBoundary.java
@@ -1,87 +1,77 @@
 package Server;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class MyServerBoundary {
-    @FXML
-    private Label lblConnectedIp;
 
-    @FXML
-    private Label lblConnectionStatus;
+	@FXML
+	private Label lblIP;
 
-    @FXML
-    private Label lblHostName;
+	@FXML
+	private Label lblStatus;
 
-    @FXML
-    private Label lblIP;
+	@FXML
+	private Label lblHost;
 
-    @FXML
-    private Label lblStatus;
+//    private SimpleStringProperty host = new SimpleStringProperty(this, "host");
 
-    @FXML
-    private Label lblHost;
+//    public String getHost() {
+//    	return host.get();
+//    }
+//    
+//    public void setHost(String host) {
+//    	this.host.set(host);
+//    	System.out.println("setHost  : " + this.host);
+//    }
 
-    private SimpleStringProperty host = new SimpleStringProperty(this, "host");
-
-    public String getHost() {
-    	return host.get();
-    }
-    
-    public void setHost(String host) {
-    	this.host.set(host);
-    	System.out.println("setHost  : " + this.host);
-    }
-   
 	public void start(Stage primaryStage) throws Exception {
-		lblHost = new Label();
-	
-		lblHost.textProperty().bind(this.host);
+//		lblHost = new Label();
+
+//		lblHost.textProperty().bind(this.host);
 		Parent root = FXMLLoader.load(getClass().getResource("/Server/MyServer.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setTitle("Server form");
 		primaryStage.setScene(scene);
 		ServerUI.runServer(ServerUI.DEFAULT_PORT);
 		primaryStage.show();
-		
-	}
-	
-	public StringProperty hostProperty() {
-		return host;
+
 	}
 
+//	public StringProperty hostProperty() {
+//		return host;
+//	}
+	public Label getIP() {
+		return lblIP;
+	}
 	public void updateServerBoundary(String host, String ip, String status) {
-		System.out.println("host - " + host);
+		lblIP = new Label();
+		Platform.runLater(() -> {
+			lblIP.setText("a");
+//			lblHost.setText("n");
+//			lblStatus.setText("c");
+		});
+//		System.out.println("update abd " + host);
+//		System.out.println("host - " + host);
+//		lblStatus.setText("abd");
 //		lblHost.textProperty().bind(this.host);
-		setHost("abcd");
-		System.out.println("this " + this.host);
-		//		System.out.println("1");
+//		setHost("abcd");
+//		System.out.println("this " + this.host);
+		// System.out.println("1");
 //		System.out.println(this.getClass().getName() + " update client status");
 //		this.host = host;
 //		this.ip=ip;
 //		this.status=status;
 //		ActionEvent event = new ActionEvent();
-		updateServerGUI();
+//		updateServerGUI();
 	}
-	
-	
-	void updateServerGUI() {
+
+//	void updateServerGUI() {
 //		this.lblHost.setText("1");
 //		this.lblIP.setText("2");
 //		this.lblStatus.setText("3");
@@ -93,14 +83,14 @@ public class MyServerBoundary {
 //		System.out.println(lblStatus);
 ////		System.out.println("ypdateservergui");
 //		lblStatus.setText("sdjjsdjdjdjdjjjdd");
-		
+
 //		lblIP.setText(ip);
 //		Platform.runLater(()->{
 //			lblStatus.setText("abbbcc");
 //	    });
-		System.out.println("update server gui ");
+//		System.out.println("update server gui ");
 //		System.out.println("text field status = " + lblIP.getText());
-	}
+}
 //
 //	@Override
 //	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -111,4 +101,3 @@ public class MyServerBoundary {
 //		lblIP = new Label("c");
 //		
 //	}
-}
