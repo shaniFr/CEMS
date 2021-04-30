96223dab3c4ed7f7ddb41462c63386a490a19e41
ayalachn <81107065+ayalachn@users.noreply.github.com>
2021-04-30 17:01:50 +0300

    server gui

--- src/Server/MyServerBoundary.java
+++ src/Server/MyServerBoundary.java
@@ -5,17 +5,21 @@ import java.util.ResourceBundle;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
-public class MyServerBoundary implements Initializable {
+public class MyServerBoundary {
     @FXML
     private Label lblConnectedIp;
 
@@ -34,11 +38,21 @@ public class MyServerBoundary implements Initializable {
     @FXML
     private Label lblHost;
 
-    private String host,ip,status;
+    private SimpleStringProperty host = new SimpleStringProperty(this, "host");
+
+    public String getHost() {
+    	return host.get();
+    }
     
-    private Stage pr;
-    Parent r;
+    public void setHost(String host) {
+    	this.host.set(host);
+    	System.out.println("setHost  : " + this.host);
+    }
+   
 	public void start(Stage primaryStage) throws Exception {
+		lblHost = new Label();
+	
+		lblHost.textProperty().bind(this.host);
 		Parent root = FXMLLoader.load(getClass().getResource("/Server/MyServer.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setTitle("Server form");
@@ -46,21 +60,32 @@ public class MyServerBoundary implements Initializable {
 		ServerUI.runServer(ServerUI.DEFAULT_PORT);
 		primaryStage.show();
 		
-		
 	}
 	
+	public StringProperty hostProperty() {
+		return host;
+	}
+
 	public void updateServerBoundary(String host, String ip, String status) {
-		System.out.println("1");
-		System.out.println(this.getClass().getName() + " update client status");
-		this.host = host;
-		this.ip=ip;
-		this.status=status;
-		ActionEvent event = new ActionEvent();
-		updateServerGUI(event);
+		System.out.println("host - " + host);
+//		lblHost.textProperty().bind(this.host);
+		setHost("abcd");
+		System.out.println("this " + this.host);
+		//		System.out.println("1");
+//		System.out.println(this.getClass().getName() + " update client status");
+//		this.host = host;
+//		this.ip=ip;
+//		this.status=status;
+//		ActionEvent event = new ActionEvent();
+		updateServerGUI();
 	}
 	
-	@FXML
-	void updateServerGUI(ActionEvent event) {
+	
+	void updateServerGUI() {
+//		this.lblHost.setText("1");
+//		this.lblIP.setText("2");
+//		this.lblStatus.setText("3");
+//		
 //		System.out.println("2");
 //		lblStatus = new Label();
 //		lblStatus.setText("something");
@@ -73,18 +98,17 @@ public class MyServerBoundary implements Initializable {
 //		Platform.runLater(()->{
 //			lblStatus.setText("abbbcc");
 //	    });
-		
+		System.out.println("update server gui ");
 //		System.out.println("text field status = " + lblIP.getText());
 	}
-
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+//
+//	@Override
+//	public void initialize(URL arg0, ResourceBundle arg1) {
 //		System.out.println("initialize..");
 //		lblStatus = new Label("a");
 //		System.out.println(lblStatus.getText());
 //		lblHost = new Label("b");
 //		lblIP = new Label("c");
 //		
-		
-	}
+//	}
 }
